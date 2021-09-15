1bff58d6f741b66d191241cad9579e807cd88e77
unknown <lidanyang1111@gmail.com>
2021-09-15 14:55:57 -0700

    version1

--- index.js
+++ index.js
@@ -39,15 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(edible){
+  if(this.stomach.length<10){
+    this.stomach.push(edible);
+  }
+};
+Person.prototype.poop =function(){
+  this.stomach.length = 0;
+};
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+};
 
-
-
-
-
-
+const mary = new Person("Mary", 50);
+console.log(mary.toString());
+mary.eat("pepperoni");
+console.log(mary.stomach);
+mary.poop();
+console.log(mary.stomach);
 
 /*
   TASK 2
@@ -63,10 +77,18 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank =0;
+  this.odometer = 0;  
 }
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
 
+}
 
 /*
   TASK 3
@@ -75,18 +97,23 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(name,age);
+  this.favoriteToy = favoriteToy; 
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding when in the global scope.
+  2. Implicit Binding whenever a preceding dot calls a function,the object before the dot is this.
+  3. New Binding whenever we use a constuctor function, this refers to the specific instance of the object that is created and returned by the constructor function.
+  4. Explicit Binding whenever we use Javascipt's call or apply method.
 */
 
 
